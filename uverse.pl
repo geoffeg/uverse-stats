2961b05fbc694ab0bd57ae4600d6914355a7609f
geoffeg <geoffeg@geoffeg.org>
2011-02-26 20:14:07 -0600

    Rewrite to use list of interfaces

--- uverse.pl
+++ uverse.pl
@@ -5,45 +5,45 @@ use HTML::Tree;
 use Data::Dumper;
 use HTML::TreeBuilder::XPath;
 
-my $url = "http://192.168.1.254/xslt?PAGE=C_2_0";
-my $content = get($url);
-#my $tree = HTML::Tree->new();
-my $tree = HTML::TreeBuilder::XPath->new;
+# Which interfaces to monitor, these MUST be in the order they appear on the web page
+my @interfaces = ("Port 1", "Port 2", "Wireless", "HomePNA1");
+my $router_ip = '192.168.1.254';
+my $images_directory = '/Users/geoffeg/Sites/stats/';
 
+###############################################################
+##### There are no user-servicable parts below this line. #####
+###############################################################
+
+my $url = "http://$router_ip/xslt?PAGE=C_2_0";
+my $content = get($url);
+
+my $tree = HTML::TreeBuilder::XPath->new;
 $tree->parse($content);
-my @ports = $tree->findvalues('/html/body//table[5]//td');
+my @td_cells = $tree->findvalues('/html/body//table[5]//td');
+
 $count = 0;
-foreach my $port (@ports) {
-	if ($port eq 'Wireless' || $port eq 'HomePNA1') {
-		$current_interface = $port;
-	}
-	if ($port =~ /Port ([0-9]+) ([a-z]+)/i) {
-		$data{"Port $1"}{$2} = $ports[$count + 1] if ($ports[$count + 1] != 0);
-	}
-	if ($current_interface eq 'Wireless' && $port eq ' Transmit') {
-		$data{'Wireless'}{'Transmit'} = $ports[$count + 1];
-	} elsif ($current_interface eq 'Wireless' && $port eq ' Receive') {
-		$data{'Wireless'}{'Receive'} = $ports[$count + 1];
-	} elsif ($current_interface eq 'HomePNA1' && $port eq ' Transmit') {
-		$data{'Coax'}{'Transmit'} = $ports[$count + 1];
-	} elsif ($current_interface eq 'HomePNA1' && $port eq ' Receive') {
-		$data{'Coax'}{'Receive'} = $ports[$count + 1];
+my $interface = shift(@interfaces);
+foreach my $td_cell (@td_cells) {
+	$current_interface = $interface if ($td_cell =~ /$interface/);
+	if ($current_interface eq $interface && $td_cell =~ /(Transmit|Receive)/) {
+		$data{$interface}{$1} = $td_cells[$count + 1];
+		$interface = shift(@interfaces) if ($1 eq "Receive");
 	}
 	$count++;
 }
 #print Dumper(%data);
 
 foreach my $port (sort keys %data) {
-print "Port: $port\n";
-	print Dumper map { ( $_ => "GAUGE" ) } keys %{ $data{$port} };
-	print Dumper map { ( $_ => $data{$port}{$_} ) } keys %{ $data{$port} };
+	#print "Port: $port\n";
+	#print Dumper map { ( $_ => "GAUGE" ) } keys %{ $data{$port} };
+	#print Dumper map { ( $_ => $data{$port}{$_} ) } keys %{ $data{$port} };
 	my $rrd = RRD::Simple->new(file => "${port}.rrd");
 	$rrd->create(map { ($_ => "COUNTER" ) } keys %{ $data{$port} }) unless -f "${port}.rrd";
 	$rrd->update("${port}.rrd", time(), map { ($_ => $data{$port}{$_}) } keys %{ $data{$port} });
 	my %rtn = $rrd->graph(
 		width => '600',
 		height => '480',
-		destination => '/Users/geoffeg/Sites/stats/',
+		destination => $images_directory,
 		title => "Network Interface $port",
 		vertical_label => 'Bytes/sec',
 		line_thickness => 2,
@@ -61,7 +61,7 @@ print "Port: $port\n";
 #		'LINE1:Receive:"download"'
 	);
 	my $info = $rrd->info;
-	print Dumper $info;
-print "\n";
+	#print Dumper $info;
+	#print "\n";
 }
 
